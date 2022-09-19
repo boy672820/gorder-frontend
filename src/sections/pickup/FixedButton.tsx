3feefb76c12ca054758bc7ad2a425ce3d5418465
Seonzoo <boy672820@gmail.com>
2022-09-19 18:58:56 +0900

    pickup button 상태 추가

--- src/sections/pickup/FixedButton.tsx
+++ src/sections/pickup/FixedButton.tsx
@@ -1,4 +1,4 @@
-import { Button, Box } from '@mui/material';
+import { Button, Box, Container } from '@mui/material';
 
 type Props = {
   isCompleted: boolean;
@@ -12,62 +12,64 @@ export default function FixedButton({ isCompleted }: Props) {
         bottom: 0,
         width: '100%',
         textAlign: 'center',
-        p: 2,
+        py: 2,
         borderTop: '1px solid',
         backgroundColor: 'white',
         borderTopColor: (theme) => theme.palette.divider,
         zIndex: 1,
       }}
     >
-      <Button
-        variant="contained"
-        color="primary"
-        size="large"
-        fullWidth
-        sx={{
-          width: '100%',
-          borderRadius: 3,
-          position: 'relative',
-          boxShadow: '0px 2px 2px rgba(0, 0, 0, 0.1)',
-          ...(isCompleted ? { backgroundColor: '#303839' } : {}),
-        }}
-      >
-        {!isCompleted && (
-          <Box
-            component="span"
-            sx={{
-              width: 24,
-              height: 24,
-              borderRadius: 12,
-              backgroundColor: 'white',
-              color: 'primary.main',
-              fontFamily: 'Apple SD Gothic Neo',
-              fontSize: (theme) => theme.typography.pxToRem(12),
-              lineHeight: (theme) => theme.typography.pxToRem(24),
-              position: 'absolute',
-              left: 16,
-            }}
-          >
-            6
-          </Box>
-        )}
+      <Container maxWidth="sm">
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          fullWidth
+          sx={{
+            width: '100%',
+            borderRadius: 3,
+            position: 'relative',
+            boxShadow: '0px 2px 2px rgba(0, 0, 0, 0.1)',
+            ...(isCompleted ? { backgroundColor: '#303839' } : {}),
+          }}
+        >
+          {!isCompleted && (
+            <Box
+              component="span"
+              sx={{
+                width: 24,
+                height: 24,
+                borderRadius: 12,
+                backgroundColor: 'white',
+                color: 'primary.main',
+                fontFamily: 'Apple SD Gothic Neo',
+                fontSize: (theme) => theme.typography.pxToRem(12),
+                lineHeight: (theme) => theme.typography.pxToRem(24),
+                position: 'absolute',
+                left: 16,
+              }}
+            >
+              6
+            </Box>
+          )}
 
-        <Box component="span">{isCompleted ? '픽업 완료하기' : '내가 픽업하기'}</Box>
+          <Box component="span">{isCompleted ? '픽업 완료하기' : '내가 픽업하기'}</Box>
 
-        {!isCompleted && (
-          <Box
-            component="span"
-            sx={{
-              position: 'absolute',
-              right: 16,
-              fontFamily: 'Apple SD Gothic Neo',
-              fontSize: (theme) => theme.typography.pxToRem(13),
-            }}
-          >
-            24,000원
-          </Box>
-        )}
-      </Button>
+          {!isCompleted && (
+            <Box
+              component="span"
+              sx={{
+                position: 'absolute',
+                right: 16,
+                fontFamily: 'Apple SD Gothic Neo',
+                fontSize: (theme) => theme.typography.pxToRem(13),
+              }}
+            >
+              24,000원
+            </Box>
+          )}
+        </Button>
+      </Container>
     </Box>
   );
 }
