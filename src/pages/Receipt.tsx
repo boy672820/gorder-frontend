1f6a8c7fd3d92090b2ad227f93d4b4ed32162d34
Seonzoo <boy672820@gmail.com>
2022-09-22 00:04:46 +0900

    stepper 완료

--- src/pages/Receipt.tsx
+++ src/pages/Receipt.tsx
@@ -3,7 +3,7 @@ import ContentContainer from '../components/ContentContainer';
 import Section from '../components/Section';
 import ReceiptInfo from '../sections/receipt/ReceiptInfo';
 import ItemList from '../sections/order/ItemList';
-import { Container, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import Stepper from '../sections/receipt/Stepper';
 
 export default function Receipt() {
@@ -14,10 +14,13 @@ export default function Receipt() {
       </Section>
 
       <Section>
-        <Container>
+        <Container maxWidth="sm">
           <Typography variant="subtitle1">주문 현황</Typography>
-          <Stepper />
         </Container>
+
+        <Box pt={1.5}>
+          <Stepper activeStep={0} />
+        </Box>
       </Section>
 
       <ContentContainer title="주문 상품">
--- src/sections/receipt/Stepper.tsx
+++ src/sections/receipt/Stepper.tsx
@@ -1,24 +1,50 @@
-import { Step, StepLabel, Stepper as MuiStepper, Typography } from '@mui/material';
+import styled from '@emotion/styled';
+import {
+  Step,
+  StepLabel as MuiStepLabel,
+  stepLabelClasses,
+  Stepper as MuiStepper,
+  Theme,
+  useTheme,
+} from '@mui/material';
 
-type StepItem = { label: string; activeLabel: string };
+type StepItem = { id: number; label: string; active: string; completed: string };
 
-const steps: Array<StepItem> = [
-  { label: '접수 대기 중', activeLabel: '접수 대기 중 입니다.' },
-  { label: '픽업 중', activeLabel: '픽업 중 입니다.' },
-  { label: '픽업완료', activeLabel: '픽업이 완료되었습니다.' },
+const stepLabel: Array<StepItem> = [
+  { id: 0, label: '접수 대기 중', active: '접수 대기 중 입니다.', completed: '접수 완료' },
+  { id: 1, label: '픽업 중', active: '픽업 중 입니다.', completed: '수령 완료' },
+  { id: 2, label: '픽업완료', active: '픽업 완료', completed: '픽업 완료' },
 ];
 
-export default function Stepper() {
-  const activeStep = 0;
+const StepLabel = styled(MuiStepLabel)(({ theme }: { theme: Theme }) => ({
+  [`& .${stepLabelClasses.label}`]: {
+    [`&.${stepLabelClasses.active}`]: {
+      color: theme.palette.primary.main,
+    },
+    width: 'max-content',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 4,
+    fontSize: theme.typography.pxToRem(11),
+  },
+}));
+
+type Props = {
+  activeStep: 0 | 1 | 2;
+};
+
+export default function Stepper({ activeStep }: Props) {
+  const theme = useTheme();
 
   return (
     <MuiStepper activeStep={activeStep} alternativeLabel>
-      {steps.map(({ label, activeLabel }: StepItem, index: number) => (
-        <Step
-          key={label}
-        >
-          {/* <Typography>{activeStep === index ? activeLabel : label}</Typography> */}
-          <StepLabel>{activeStep === index ? activeLabel : label}</StepLabel>
+      {stepLabel.map(({ id, label, active, completed }: StepItem, index: number) => (
+        <Step key={id}>
+          <StepLabel theme={theme}>
+            {activeStep < index && label}
+            {activeStep === index && active}
+            {activeStep > index && completed}
+          </StepLabel>
         </Step>
       ))}
     </MuiStepper>
